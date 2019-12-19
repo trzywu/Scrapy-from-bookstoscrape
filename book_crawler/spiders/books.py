c3923d8c92bd8c4d60ff5cb038a42f45c7676d96
Tomasz Watroba <trzywu94@gmail.com>
2019-12-19 23:37:19 +0100

    saving to csv
    Added automated save to .csv file

--- book_crawler/spiders/books.py
+++ book_crawler/spiders/books.py
@@ -1,66 +1,57 @@
 # -*- coding: utf-8 -*-
-from scrapy import  Spider
-from scrapy.http import  Request
+from scrapy import Spider
+from scrapy.http import Request
+import csv
 
 
+def productinfo(response, value):
+    return response.xpath('//th[text()="' + value + '"]/following-sibling::td/text()').extract_first()
 
 
 class BookSpider(Spider):
     name = 'books'
     allowed_domains = ['books.toscrape.com']
     start_urls = ['http://books.toscrape.com']
+    output = "books.csv"
+    row_name = ['Title', 'Price', 'Full_image_url', 'Rating', 'Product_desription', 'UPC', 'Product_type', 'Price_without_tax', 'Price_with_tax', 'Tax','Availability', 'Nr_reviews']
+    with open(output, "w", newline='') as myfile:
+            wr = csv.writer(myfile)
+            wr.writerow(row_name)
 
-    def parse(self,response):
+
+
+    def parse(self, response):
         books = response.xpath('//h3/a/@href').extract()
 
         for book in books:
             absolute_url = response.urljoin(book)
-            yield  Request(absolute_url, callback= self.parse_book)
+            yield Request(absolute_url, callback=self.parse_book)
 
-
-        #getting to next page
+        # getting to next page
         next_page_url = response.xpath('//a[text()="next"]/@href').extract_first()
         absolute_url = response.urljoin(next_page_url)
-        yield  Request(absolute_url)
+        yield Request(absolute_url)
 
-    def product_info(response, value):
-        return response.xpath('//th[text()="' + value + '"]/following-sibling:td/text()').extract_first()
-
-
-    def parse_book(self,response):
+    def parse_book(self, response):
         title = response.css('h1::text').extract_first()
         price = response.xpath('//*[@class="price_color"]/text()').extract_first()
         image_url = response.xpath('//img/@src').extract_first()
         full_image_url = image_url.replace('../..', 'http://books.toscrape.com/')
         rating = response.xpath('//*[contains(@class, "star-rating")]/@class').extract_first()
         rating = rating.replace('star-rating ', '')
-        product_desription = response.xpath('//*[@id="product_description"]/following-sibling::p').extract_first()
-        # upc = product_info(response,'UPC')
-        # product_type = product_info(response,'Product Type')
-        # price_without_tax = product_info(response, 'Price (excl. tax)')
-        # price_with_tax = product_info(response, 'Price (incl. tax)')
-        # Tax = product_info(response, 'Tax')
-        # Availability = product_info(response, 'Availability')
-        # number_of_review = product_info(response,'Number of reviews')
+        product_desription = response.xpath('//*[@id="product_description"]/following-sibling::p/text()').extract_first()
+        upc = productinfo(response, 'UPC')
+        product_type = productinfo(response, 'Product Type')
+        price_without_tax = productinfo(response, 'Price (excl. tax)')
+        price_with_tax = productinfo(response, 'Price (incl. tax)')
+        tax = productinfo(response, 'Tax')
+        availability = productinfo(response, 'Availability')
+        nr_reviews = productinfo(response, 'Number of reviews')
 
-        yield {
-            'title' : title,
-            'price' : price,
-            'Image_URL' : full_image_url,
-            'Ratimg' : rating,
-            'Product Descritpion' : product_desription,
-            # 'UPC': upc,
-            # 'Product Type': product_type,
-            # 'Price (excl. tax)' : price_without_tax,
-            # 'Price (incl. tax)' : price_with_tax,
-            # 'Tax': Tax,
-            # 'Availability' : Availability,
-            # 'Number of reviews' : number_of_review
-        }
-
-
-
-
+        with open(self.output, "a", newline='') as myfile:
+            wr = csv.writer(myfile)
+            wr.writerow([title, price, full_image_url, rating, product_desription, upc, product_type, price_without_tax, price_with_tax, tax,availability, nr_reviews])
+ 
 
 
 
